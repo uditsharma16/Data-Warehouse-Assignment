c18cd80f177789225c19a085e6fb04304bc8705c
uditsharma16 <uditsharma16@yahoo.com>
2020-06-05 17:58:11 +1200

    Update queriesDW.sql
    Adding the date_table to queries.

--- queriesDW.sql
+++ queriesDW.sql
@@ -1,7 +1,7 @@
 ---Task 1---------------------------------------
 select * from (select p.product_name, sum(tf.total_sale) as "TOTAl_SALE",rank() over(order by sum(tf.total_sale) desc) as "RANK"
-from product p,transactions_fact tf
-where p.product_id=tf.product_id and extract(month from tf.t_date)=12 
+from product p,transactions_fact tf,date_table dt
+where p.product_id=tf.product_id and  tf.date_id=dt.date_id and dt.month_num=12
 group by p.product_name
 ) where rownum<6;
 
@@ -12,28 +12,24 @@ where s.store_id=tf.store_id
 group by s.store_name) where rownum=1;
 
 ----Task 3------------------------------------------------------------------------
-select p.product_name, sum(tf.total_sale) as "TOTAl_SALE",rank() over(order by sum(tf.total_sale) desc)||' '||extract(month from tf.t_date) as "RANK"
-from product p,transactions_fact tf
-where p.product_id=tf.product_id and extract(month from tf.t_date) in (12,11,10) 
-group by p.product_name,extract(month from tf.t_date)
-order by extract(month from tf.t_date) desc ;
 
-select * from (select p.product_name, sum(tf.total_sale) as "TOTAl_SALE",rank() over(order by sum(tf.total_sale) desc)||' '||extract(month from tf.t_date) as "RANK"
-from product p,transactions_fact tf
-where p.product_id=tf.product_id and extract(month from tf.t_date)=12 
-group by p.product_name,extract(month from tf.t_date)
+
+select * from (select p.product_name, sum(tf.total_sale) as "TOTAl_SALE",rank() over(order by sum(tf.total_sale) desc)||' '||dt.month as "RANK"
+from product p,transactions_fact tf,date_table dt
+where p.product_id=tf.product_id and dt.month_num=@EmpIDVar and tf.date_id=dt.date_id
+group by p.product_name,dt.month
 ) where rownum<4
 union
-select * from (select p.product_name, sum(tf.total_sale) as "TOTAl_SALE",rank() over(order by sum(tf.total_sale) desc)||' '||extract(month from tf.t_date) as "RANK"
-from product p,transactions_fact tf
-where p.product_id=tf.product_id and extract(month from tf.t_date)=11 
-group by p.product_name,extract(month from tf.t_date)
+select * from (select p.product_name, sum(tf.total_sale) as "TOTAl_SALE",rank() over(order by sum(tf.total_sale) desc)||' '||dt.month as "RANK"
+from product p,transactions_fact tf,date_table dt
+where p.product_id=tf.product_id and dt.month_num=@EmpIDVar-1 and tf.date_id=dt.date_id
+group by p.product_name,dt.month
 ) where rownum<4
 union
-select * from (select p.product_name, sum(tf.total_sale) as "TOTAl_SALE",rank() over(order by sum(tf.total_sale) desc)||' '||extract(month from tf.t_date) as "RANK"
-from product p,transactions_fact tf
-where p.product_id=tf.product_id and extract(month from tf.t_date)=10 
-group by p.product_name,extract(month from tf.t_date)
+select * from (select p.product_name, sum(tf.total_sale) as "TOTAl_SALE",rank() over(order by sum(tf.total_sale) desc)||' '||dt.month as "RANK"
+from product p,transactions_fact tf,date_table dt
+where p.product_id=tf.product_id and dt.month_num=@EmpIDVar-2 and tf.date_id=dt.date_id
+group by p.product_name,dt.month
 ) where rownum<4;
 
 ----Task 4---------------------------------------------------
@@ -43,5 +39,21 @@ from product p,store s,transactions_fact tf
 where tf.store_id=s.store_id and tf.product_id=p.product_id
 group by s.store_id,p.product_id
 order by s.store_id,p.product_id;
+
+
+
+
+--select p.product_name, sum(tf.total_sale) as "TOTAl_SALE",rank() over(order by sum(tf.total_sale) desc)||' '||extract(month from tf.t_date) as "RANK"
+--from product p,transactions_fact tf,date_table dt
+--where p.product_id=tf.product_id and dt.date_id=tf.date_id and dt.month_num in(12,11,10)
+--group by p.product_name,extract(month from tf.t_date)
+--order by extract(month from tf.t_date) desc ;
+
+select p.product_name, sum(tf.total_sale) ,dt.month,rank() over(order by sum(tf.total_sale) desc)
+from product p,transactions_fact tf,date_table dt
+where p.product_id=tf.product_id  and tf.date_id=dt.date_id and 
+dt.month_num in (select * from (select unique(month_num) from date_table where month_num<=&month order by month_num desc) where rownum<=3)
+group by dt.month,p.product_name;
+
 
 
